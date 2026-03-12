b8de3e83092f9c26eea59b8f307b7c1a362e8dd9
GuBidin <gustavobidin89@gmail.com>
2026-03-12 14:08:55 -0300

    ex1 ex2 ex3

---
+++ main.py
@@ -0,0 +1,33 @@
+nome = input("Digite seu nome: ")
+print(f"Olá, {nome}! Bem-vindo ao Python!")
+#float: números decimais
+#int: números inteiros
+#string: texto
+#variáveis: são usadas para armazenar valores
+
+
+#fazendo operações matemáticas
+valor1 = float(input("Digite o primeiro valor: "))
+valor2 = float(input("Digite o segundo valor: "))
+soma = valor1 + valor2
+print(f"A soma de {valor1} e {valor2} é: {soma}")
+subtracao = valor1 - valor2
+print(f"A subtração de {valor1} e {valor2} é: {subtracao}")
+multiplicacao = valor1 * valor2
+print(f"A multiplicação de {valor1} e {valor2} é: {multiplicacao}")
+if valor2 != 0:
+    divisao = valor1 / valor2
+    print(f"A divisão de {valor1} por {valor2} é: {divisao}")   
+else:    print("Não é possível dividir por zero.")
+
+
+# Calculando a área de um triângulo
+base = float(input("Digite a base do triângulo: "))
+altura = float(input("Digite a altura do triângulo: "))
+area_triangulo = (base * altura / 2)
+print(f"A área do triângulo é: {area_triangulo}")
+
+#convertendo temperatura de Celsius para Fahrenheit
+celsius = float(input("Digite a temperatura em Celsius: "))
+fahrenheit = (celsius * 9/5) + 32
+print(f"A temperatura em Fahrenheit é: {fahrenheit}")
